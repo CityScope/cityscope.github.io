c4355a9f5111881cf0af802e387714af15173975
Yasushi Sakai <yasushisakai@users.noreply.github.com>
2020-04-12 19:51:44 -0400

    custom ghpages  config to point to `master` branch
    Github Pages for org does not use `gh_pages` branch when deployed.
    This builds it to the `master` branch. In the same time the repo should
    set the base branch as something else.

--- cityscope/src/index.js
+++ cityscope/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import ghpages
+
+ghpages.publish('doc', {
+  branch: 'master',
+  repo: 'https://github.com/CityScience/cityscope.github.io.git'
+}, function(err){});
 
 ReactDOM.render(
     <React.StrictMode>
