648e0d192f5c7e6fd91c89ee5683b56cdf47c9f0
crisjf <crisjf@mit.edu>
2020-09-18 17:22:47 -0400

    update action to put docs in modules instead of backend

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -9,19 +9,24 @@
 #   * DESTINATION_PATH: Path in local repo where to store the md file (it defaults to docsite/docs/general)
 #   * SOURCE_FILE_PATH: Path to fetch, including name of file, from remote repo (it defaults to master/README.md)
 
-echo "Copying docs from: ${2}/${4:-master/README.md}"
-echo "Copying docs into: ${3:-docsite/docs/general}/${2}.md"
-echo "Downloading file at https://raw.githubusercontent.com/${1}/${2}/${4:-master/README.md}"
-mkdir -p ${3:-docsite/docs/general}/
-curl -o ${3:-docsite/docs/general}/${2}_raw.md https://raw.githubusercontent.com/${1}/${2}/${4:-master/README.md}
-head ${3:-docsite/docs/general}/${2}_raw.md
-touch ${3:-docsite/docs/general}/${2}_id.md
-echo "---" >> ${3:-docsite/docs/general}/${2}_id.md
-echo "id: ${2}" >> ${3:-docsite/docs/general}/${2}_id.md
-echo "---" >> ${3:-docsite/docs/general}/${2}_id.md
-head ${3:-docsite/docs/general}/${2}_id.md
-cat ${3:-docsite/docs/general}/${2}_id.md ${3:-docsite/docs/general}/${2}_raw.md > ${3:-docsite/docs}/${2}.md
-head ${3:-docsite/docs}/${2}.md
-ls -la ${3:-docsite/docs/general}/
-rm ${3:-docsite/docs/general}/${2}_id.md
-rm ${3:-docsite/docs/general}/${2}_raw.md
+REPO_NAME_OWNER=${1}
+REPO_NAME=${2}
+REPO_LABEL=${REPO_NAME#"CS_"}
+DESTINATION_PATH=${3:-docsite/docs/general}/${REPO_LABEL}
+SOURCE_FILE_PATH=${4:-master/README.md}
+echo "Copying docs from: ${REPO_NAME}/${SOURCE_FILE_PATH}"
+echo "Copying docs into: ${DESTINATION_PATH}/${REPO_NAME}.md"
+echo "Downloading file at https://raw.githubusercontent.com/${REPO_NAME_OWNER}/${REPO_NAME}/${SOURCE_FILE_PATH}"
+mkdir -p ${DESTINATION_PATH}/
+curl -o ${DESTINATION_PATH}/${REPO_NAME}_raw.md https://raw.githubusercontent.com/${REPO_NAME_OWNER}/${REPO_NAME}/${SOURCE_FILE_PATH}
+head ${DESTINATION_PATH}/${REPO_NAME}_raw.md
+touch ${DESTINATION_PATH}/${REPO_NAME}_id.md
+echo "---" >> ${DESTINATION_PATH}/${REPO_NAME}_id.md
+echo "id: ${REPO_LABEL}" >> ${DESTINATION_PATH}/${REPO_NAME}_id.md
+echo "---" >> ${DESTINATION_PATH}/${REPO_NAME}_id.md
+head ${DESTINATION_PATH}/${REPO_NAME}_id.md
+cat ${DESTINATION_PATH}/${REPO_NAME}_id.md ${DESTINATION_PATH}/${REPO_NAME}_raw.md > ${DESTINATION_PATH}/${REPO_NAME}.md
+head ${DESTINATION_PATH}/${REPO_NAME}.md
+ls -la ${DESTINATION_PATH}/
+rm ${DESTINATION_PATH}/${REPO_NAME}_id.md
+rm ${DESTINATION_PATH}/${REPO_NAME}_raw.md
