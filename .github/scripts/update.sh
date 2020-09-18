7b4fe61fe2158faedf8e8afc6470bb26b8007032
crisjf <crisjf@mit.edu>
2020-09-18 17:24:45 -0400

    store docs in folder with repo name inside given path

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -15,18 +15,18 @@ REPO_LABEL=${REPO_NAME#"CS_"}
 DESTINATION_PATH=${3:-docsite/docs/general}/${REPO_LABEL}
 SOURCE_FILE_PATH=${4:-master/README.md}
 echo "Copying docs from: ${REPO_NAME}/${SOURCE_FILE_PATH}"
-echo "Copying docs into: ${DESTINATION_PATH}/${REPO_NAME}.md"
+echo "Copying docs into: ${DESTINATION_PATH}/${REPO_LABEL}.md"
 echo "Downloading file at https://raw.githubusercontent.com/${REPO_NAME_OWNER}/${REPO_NAME}/${SOURCE_FILE_PATH}"
 mkdir -p ${DESTINATION_PATH}/
-curl -o ${DESTINATION_PATH}/${REPO_NAME}_raw.md https://raw.githubusercontent.com/${REPO_NAME_OWNER}/${REPO_NAME}/${SOURCE_FILE_PATH}
-head ${DESTINATION_PATH}/${REPO_NAME}_raw.md
-touch ${DESTINATION_PATH}/${REPO_NAME}_id.md
-echo "---" >> ${DESTINATION_PATH}/${REPO_NAME}_id.md
-echo "id: ${REPO_LABEL}" >> ${DESTINATION_PATH}/${REPO_NAME}_id.md
-echo "---" >> ${DESTINATION_PATH}/${REPO_NAME}_id.md
-head ${DESTINATION_PATH}/${REPO_NAME}_id.md
-cat ${DESTINATION_PATH}/${REPO_NAME}_id.md ${DESTINATION_PATH}/${REPO_NAME}_raw.md > ${DESTINATION_PATH}/${REPO_NAME}.md
-head ${DESTINATION_PATH}/${REPO_NAME}.md
+curl -o ${DESTINATION_PATH}/${REPO_LABEL}_raw.md https://raw.githubusercontent.com/${REPO_NAME_OWNER}/${REPO_NAME}/${SOURCE_FILE_PATH}
+head ${DESTINATION_PATH}/${REPO_LABEL}_raw.md
+touch ${DESTINATION_PATH}/${REPO_LABEL}_id.md
+echo "---" >> ${DESTINATION_PATH}/${REPO_LABEL}_id.md
+echo "id: ${REPO_LABEL}" >> ${DESTINATION_PATH}/${REPO_LABEL}_id.md
+echo "---" >> ${DESTINATION_PATH}/${REPO_LABEL}_id.md
+head ${DESTINATION_PATH}/${REPO_LABEL}_id.md
+cat ${DESTINATION_PATH}/${REPO_LABEL}_id.md ${DESTINATION_PATH}/${REPO_LABEL}_raw.md > ${DESTINATION_PATH}/${REPO_LABEL}.md
+head ${DESTINATION_PATH}/${REPO_LABEL}.md
 ls -la ${DESTINATION_PATH}/
-rm ${DESTINATION_PATH}/${REPO_NAME}_id.md
-rm ${DESTINATION_PATH}/${REPO_NAME}_raw.md
+rm ${DESTINATION_PATH}/${REPO_LABEL}_id.md
+rm ${DESTINATION_PATH}/${REPO_LABEL}_raw.md
