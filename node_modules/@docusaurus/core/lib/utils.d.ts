7f57ba8a422b049ebc56fc78250c1e9e4cddbc3b
Ariel Noyman <arielnoyman@gmail.com>
2024-04-30 21:01:48 -0400

    cleanup

--- node_modules/@docusaurus/core/lib/utils.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const PerfDebuggingEnabled: boolean;
-type PerfLoggerAPI = {
-    start: (label: string) => void;
-    end: (label: string) => void;
-    log: (message: string) => void;
-    async: <Result>(label: string, asyncFn: () => Result | Promise<Result>) => Promise<Result>;
-};
-export declare const PerfLogger: PerfLoggerAPI;
-export {};
